17d72fe19afecfd27273a5e17d8e554e9b66f1ca
Zoooe-Brooo <zhongwenli.zoe@gmail.com>
2024-10-02 18:41:51 +0930

    Update routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,32 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/userController');
 
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').delete(removeFriend);
+
+// /api/users/:userId/reactions
+router.route('/:userId/reactions').post(addReaction);
+
+// /api/users/:userId/reactions/:reactionId
+router.route('/:userId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
